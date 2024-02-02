8b5ce708b46f3f30510e1a8364c6bc6881ed10bc
bob walker <bob@randomness.org.uk>
2024-02-02 16:23:21 +0000

    Use double quotes since the standard says so.
    ```
     standard: Use Ruby Standard Style (https://github.com/standardrb/standard)
      spec/patterdale/ical_spec.rb:11:78: Style/StringLiteralsInInterpolation: Prefer double-quoted strings inside interpolations.
      spec/patterdale/ical_spec.rb:12:84: Style/StringLiteralsInInterpolation: Prefer double-quoted strings inside interpolations.
    ```

--- spec/patterdale/ical_spec.rb
+++ spec/patterdale/ical_spec.rb
@@ -8,8 +8,8 @@ describe Patterdale::ICal do
 
     it "returns an iCal feed" do
       rota.each do |r|
-        expect(results).to match(/DTSTART;VALUE=DATE:#{r.start_date.strftime('%Y%m%d')}/)
-        expect(results).to match(/DTEND;VALUE=DATE:#{(r.end_date + 1.day).strftime('%Y%m%d')}/)
+        expect(results).to match(/DTSTART;VALUE=DATE:#{r.start_date.strftime("%Y%m%d")}/)
+        expect(results).to match(/DTEND;VALUE=DATE:#{(r.end_date + 1.day).strftime("%Y%m%d")}/)
         expect(results).to match(/SUMMARY:#{r.developer_name} and #{r.ops_name} on support/)
       end
     end
